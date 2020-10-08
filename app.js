aa6f4bd779b359354abd441f7fa3f14d96f321fb
Bogdan <buselb2@gmail.com>
2020-10-08 16:43:19 +0300

    display cart item

--- app.js
+++ app.js
@@ -78,10 +78,12 @@ class UI {
       // set cart values
       this.setCartValues(cart);
       // add cart item
+      this.addCartItem(cartItem);
       // show the cart
     });         
   });
  }
+
  setCartValues(cart) {
    let tempTotal = 0;
    let itemsTotal = 0;
@@ -91,7 +93,26 @@ class UI {
    });
    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
    cartItems.innerText = itemsTotal;
-   console.log(cartTotal, cartItems);
+ }
+
+ addCartItem(item) {
+   const div = document.createElement('div');
+   div.classList.add('cart-item');
+   div.innerHTML = `
+   <img src=${item.image} alt="product">
+   <div>
+    <h4>${item.title}</h4>
+    <h5>$${item.price}</h5>
+    <span class="remove-item" data-id=${item.id}>remove</span>
+   </div>
+   <div>
+    <i class="fas fa-chevron-up" data-id=${item.id}></i>
+    <p class="item-amount">${item.amount}</p>
+    <i class="fas fa-chevron-down" data-id=${item.id}></i>
+   </div>
+   `
+   cartContent.appendChild(div);
+   console.log(cartContent);
  }
 }
 
